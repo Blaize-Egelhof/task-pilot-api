929d5a39388f3cde385171a9b9908d15bdeee9af
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-06-17 10:53:29 +0000

    fixed profile model image url

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     bios = models.TextField(blank=True, default='Create A Bios!')
     image = models.ImageField(
         upload_to='images/',
-        default='/v1715765148/taskpilot/ffomfbsj8j1wjaiqi5r5.jpg>', blank=True
+        default='/v1715765148/taskpilot/ffomfbsj8j1wjaiqi5r5.jpg', blank=True
     )
 
     class Meta:
