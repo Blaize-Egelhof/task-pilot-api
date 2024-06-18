604ed7fb831c24bc688f5c7fd73f2139f18fffb5
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-06-18 13:34:27 +0000

    feat! added log out functionality to back-end

--- taskpilot/urls.py
+++ taskpilot/urls.py
@@ -5,16 +5,17 @@ from task import urls
 from inbox import urls
 from user_messages import urls
 from task_message import urls
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('profiles.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
         path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
+    path('dj-rest-auth/logout/', logout_route),
+    path('', include('profiles.urls')),
     path('', include('task.urls')),
     path('', include('inbox.urls')),
     path('', include('user_messages.urls')),
