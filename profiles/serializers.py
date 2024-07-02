94d29ef368b69006ce551acd142ca90c257fc712
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-07-02 08:50:16 +0000

    fix! issues displaying owned and joine task counts for profile model

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -38,7 +38,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'bios', 'image',
-            'public_tasks_count', 'joined_tasks_count', 'is_owner'
+            'owned_tasks_count', 'joined_tasks_count', 'is_owner'
         ]
     read_only_fields = ['id', 'owner', 'created_at', 'is_owner']
 
