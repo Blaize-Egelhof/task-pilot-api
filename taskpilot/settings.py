a0ca336620194e95d26924a9e4e8fb241406d97e
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-08-15 12:18:55 +0000

    switched off debug

--- taskpilot/settings.py
+++ taskpilot/settings.py
@@ -22,8 +22,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
-# 'DEV' in os.environ
+DEBUG = 'DEV' in os.environ
+
 
 ALLOWED_HOSTS = [
     '8000-blaizeegelh-taskpilotap-pguh9ahf0x4.ws-eu114.gitpod.io',
